a11da0a61edb482feb76ee168a203c664ea7e9f4
Mix2s <44645718+Mix2s@users.noreply.github.com>
2019-02-26 21:46:03 +0800

    Create SelectSort.java

---
+++ 2019 Test/February/2_26/SelectSort.java
@@ -0,0 +1,35 @@
+package day_2_26;
+/*
+ * 先将第一个位值上的数跟之后所有位置上的数依次进行比较，
+ * 如果第一个位置上的数比第二个位置上的数大，则进行互换，
+ * 然后继续将第一个位置上的数与第三个位置上的数进行比较，
+ * 经过一轮的比较后，第一个位值上的数就是所有数中最小的一个，
+ * 接着将第二个位置上的数与之后所有位置上的数进行比较，
+ * 同样的规则，第二轮比较结束后，
+ * 第二位放的就是所有数中第二小的数，
+ * 依次往下比，直到最后一个位置结束。
+ * 按照这种方法进行排序，就叫做选择排序。
+ */
+public class SortTest {
+public void selectSort(int[] array) {
+	for(int i=0;i<array.length;i++) {
+		for(int j=i+1;j<array.length;j++) {
+			if(array[i]>array[j]) {
+				int t=array[i];
+				array[i]=array[j];
+				array[j]=t;
+			}
+		}
+	}
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array= {2,3,5,1,7,0};
+		SortTest st = new SortTest();
+		st.selectSort(array);
+		for(int i:array) {
+			System.out.println("i"+i);
+		}
+	}
+
+}
