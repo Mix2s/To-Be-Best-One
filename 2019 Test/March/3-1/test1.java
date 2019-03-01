29b6ecb6c93fd9408eddf653194562316a9195aa
Mix2s <44645718+Mix2s@users.noreply.github.com>
2019-03-01 20:47:01 +0800

    Create test1.java

---
+++ 2019 Test/March/3-1/test1.java
@@ -0,0 +1,67 @@
+// public class 测试{
+// public static void main(String[] args){
+// int 变量1 = 10;
+// int 变量2 = 10;
+// int 计算结果 = 变量1 * 变量2;
+// System.out.println(计算结果);
+// }
+// }
+class tt{
+	public int a;
+}
+public class test{
+	
+	public static void main(String []args){
+		// int maxIntValue=Integer.MAX_VALUE;
+		// int minValue=Integer.MIN_VALUE;
+		// int num=maxIntValue+1;
+		// long num1=maxIntValue+1L;
+		// System.out.println(num1);
+		// System.out.println(num+1L);
+		// System.out.println(minValue-1);
+		
+		// int num;
+		// System.out.println(num);
+		// //未初始化变量num
+		// tt tt = new tt();
+		// System.out.println(tt.a);  
+		
+		
+		// int num=10;
+		// byte date=(byte)num;
+		// byte date1=(byte)300;
+		// System.out.println(date1);
+		// //44
+		// System.out.println(date);
+		// //10
+		
+		// double num=10.2;
+		// System.out.println(num*2);
+		// //20.4
+		
+		// //float f1=1.1f;
+		// System.out.println(1.1*1.1);
+		// //1.2100000000000002
+		
+		// int numA=10;
+		// int numB=4;
+		// System.out.println(numA/numB);  //2
+		// System.out.println(numA/(double)numB);  //2.5
+		
+		// char c='a';
+		// int c1=c;
+		// System.out.println(c1); //97
+		
+		int a=0;
+		for(int i=0;i<5;i++){
+			a=a++;
+		}
+		System.out.println(a);
+		
+		
+		if(1<2||(10/0==0)){
+			System.out.println("wudi");
+		}
+	}
+}
+		
