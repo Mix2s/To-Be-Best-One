8c6e810ce7f3e71b017bdcc83cf99d04af6edf8f
Mix2s <44645718+Mix2s@users.noreply.github.com>
2019-03-05 19:01:37 +0800

    Update test2.java

--- 2019 Test/March/3-4/test2.java
+++ 2019 Test/March/3-4/test2.java
@@ -1,1 +1,26 @@
-//TDDD
+public class test{
+	public static void carry(TalkPower p, String input) { 
+	System.out.println("调用对象名：" + p.getClass().getName()); 
+	System.out.println(p.talk(input)); 
+	}
+	public static void main(String[] args) {
+        carry(new People(), TestUtils.GMS);
+        carry(new Anima(), TestUtils.GMS);
+    }
+}
+interface TalkPower {
+    String talk(String input);
+}
+class People implements TalkPower {
+
+    @Override
+    public String talk(String input) {
+        return input.toUpperCase();
+    }
+}
+class Anima implements TalkPower {
+	@Override
+	public String talk(String input) { 
+	return Arrays.toString(input.split(" ")); 
+	} 
+	}
