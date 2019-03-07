0c2799bb1dbab0a5049f5e0864d7e23b4d593b44
Mix2s <44645718+Mix2s@users.noreply.github.com>
2019-03-07 22:13:01 +0800

    Update test1.java

--- 2019 Test/March/3-6/test1.java
+++ 2019 Test/March/3-6/test1.java
@@ -1,1 +1,23 @@
-//TDDD
+class Person{
+private static String country;
+private String name;
+private int age;
+public Person(String name,int age){
+this.name = name ;
+this.age = age ;
+}
+public static void setCountry(String c){
+country = c ;
+}
+public void getPersonInfo(){
+System.out.println("姓名:"+this.name+",年龄："+this.age+"，国家："+this.country);
+}
+}
+
+public class test{
+public static void main(String[] args) {
+Person.setCountry("中国");
+Person person = new Person("张三",20);
+person.getPersonInfo();
+}
+}
