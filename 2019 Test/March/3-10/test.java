72c5d8f7f27b5801eb2a534591bb7db9694af911
Mix2s <44645718+Mix2s@users.noreply.github.com>
2019-03-10 20:29:56 +0800

    Create test.java

---
+++ 2019 Test/March/3-10/test.java
@@ -0,0 +1,81 @@
+
+	public class test{
+		public static void main(String[] args){
+			Outer out =  new Outer();
+			out.display(20);
+			//code
+			// Outer.Inner inner = new Outer().new Inner();
+			// inner.display();
+			
+			// Outer out = new Outer();
+			// out.display();
+			
+			// Outer.Inner in = new Outer.Inner();
+			// in.print();
+		}
+			
+	public static void code(){
+		
+		// interface MyInterface{
+	// void test();
+// }
+// class Outer{
+	// private int num;
+	// public void display(int para){
+		// new MyInterface(){
+			// public void test(){
+				// System.out.println("匿名"+para);
+			// }
+		// }.test();
+	// }
+// }
+
+		// private int num;
+	// public void display(int test){
+		// class Inner{
+			// private void fun(){
+				// num++;
+				// System.out.println(num);
+				// System.out.println(test);
+			// }
+		// }
+		// new Inner().fun();
+	// }
+	
+	// private String outName;
+	// private int outAge;
+	// class Inner{
+		// private int InnerAge;
+		// public Inner(){
+			// Outer.this.outName="wudiwudi";
+			// Outer.this.outAge=20;
+		// }
+		// public void display(){
+			// System.out.println(outName);
+			// System.out.println(outAge);
+		// }
+	// }
+	
+	// public void display(){
+		// Inner inner = new Inner();
+		// inner.display();
+	// }
+	// class Inner{
+		// public void display(){
+			// System.out.println("wududwuwdu");
+		// }
+	// }
+	
+		// private static String msg = "wudiwu";
+	// static class Inner{
+		// public void print(){
+			// System.out.println(msg);
+		// }
+	// }
+	// public void fun(){
+		// Inner in = new Inner();
+		// in.print();
+	// }
+}
+}
+	
