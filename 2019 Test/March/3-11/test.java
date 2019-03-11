530a7459e02d67aa56f77d9f3d4123fe3572f5fb
Mix2s <44645718+Mix2s@users.noreply.github.com>
2019-03-11 19:00:32 +0800

    Create test.java

---
+++ 2019 Test/March/3-11/test.java
@@ -0,0 +1,61 @@
+public class test {
+	static {
+		System.out.println("test static 1");
+	}
+	static {
+		System.out.println("test static 2");
+	}
+    public static void main(String[] args) {
+ 
+    }
+public static void code(){
+	       // new MyClass();
+		/**
+		test static
+		myclass static
+		person static
+		person Test
+		test constructor
+		person MyClass
+		myclass constructor
+		**/
+	
+	    // Person person = new Person("Test");
+    // static{
+        // System.out.println("1 test static");
+    // }
+     
+    // public test() {
+        // System.out.println("5 test constructor");
+    // }
+ // static{
+        // System.out.println("3 person static");
+    // }
+    // public Person(String str) {
+        // System.out.println("4 6person "+str);
+    // }
+	
+// class Person{
+    // static{
+        // System.out.println("3 person static");
+    // }
+    // public Person(String str) {
+        // System.out.println("4 6person "+str);
+    // }
+// }
+ 
+ 
+// class MyClass extends test {
+    // Person person = new Person("MyClass");
+    // static{
+        // System.out.println("2 myclass static");
+    // }
+     
+    // public MyClass() {
+        // System.out.println("7 myclass constructor");
+    // }
+// }
+}
+}
+
+ 
