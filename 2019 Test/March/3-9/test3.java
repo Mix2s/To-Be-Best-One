80c0bdf19b002a78fdd48cdf383335972a14acd7
Mix2s <44645718+Mix2s@users.noreply.github.com>
2019-03-13 22:39:09 +0800

    Update test3.java

--- 2019 Test/March/3-9/test3.java
+++ 2019 Test/March/3-9/test3.java
@@ -1,1 +1,39 @@
-//TDDD
+import java.util.Scanner;
+interface Computer{
+	void printComputer();
+}
+class MacComputer implements Computer{
+	public void printComputer(){
+		System.out.println("This is MacComputer");
+	}
+}
+class WinComputer implements Computer{
+	public void printComputer(){
+		System.out.println("This is WinComputer");
+	}
+}
+class ComputerFactory{
+	public static Computer getInstance(String type){
+		switch(type){
+			case "Mac":
+				return new MacComputer();
+			case "Win":
+				return new WinComputer();
+			default:
+				return null;
+		}
+	}
+}
+public class Test{
+	public void buyComputer(Computer computer){
+		computer.printComputer();
+	}
+	public static void main(String[] args){
+		Test test = new Test();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("wudi8");
+		String type = scanner.next();
+		Computer computer = ComputerFactory.getInstance(type);
+		test.buyComputer(computer);
+	}
+}
