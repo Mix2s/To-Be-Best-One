452da587090a018b53058ac9c71996e50dc7bc75
Mix2s <44645718+Mix2s@users.noreply.github.com>
2019-03-19 14:48:17 +0800

    Update 2.java

--- 2019 Test/March/3-18/2.java
+++ 2019 Test/March/3-18/2.java
@@ -1,1 +1,42 @@
 、、tt
+import java.util.Scanner;
+interface Car{
+	void createCar();          //汽车总接口 汽车这一类
+}
+
+class BMW implements Car{          //BMW
+	public void createCar(){
+		System.out.println("生产一个BMW");
+	}
+}
+class AuDi implements Car{			//AuDi
+	public void createCar(){
+		System.out.println("生产一个AuDi");
+	}
+}
+
+interface Factory{
+	Car createCar();
+}
+
+class BMW4S implements Factory{
+	public Car createCar(){
+		return new BMW();
+	}
+}
+class AuDi4s implements Factory{
+	public Car createCar(){
+		return new AuDi();
+	}
+}
+public class Test{
+	public void buyCar(Car car){
+		car.createCar();				//根据对象打印适合方法
+	}
+	public static void main(String[] argss){
+		Test test = new Test();
+		Factory factory = new BMW4S();
+		test.buyCar(factory.createCar());
+		
+	}
+}
