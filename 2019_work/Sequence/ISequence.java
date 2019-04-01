9374e39143c048a109617dae879d6ea80c833731
Mix2s <44645718+Mix2s@users.noreply.github.com>
2019-04-01 21:46:37 +0800

    Create ISequence.java

---
+++ 2019_work/Sequence/ISequence.java
@@ -0,0 +1,12 @@
+package com.shuihui.impl;
+
+public interface ISequence {
+    boolean add(int pos,Object data);
+    int search(Object key);
+    boolean contains(Object key);
+    Object getPos(int pos);
+    Object remove(Object key);
+    int size();
+    void display();
+    void clear();
+}
