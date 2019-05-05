5d15dacebaa7aba810c47a647152a73a79aecc00
Mix2s <44645718+Mix2s@users.noreply.github.com>
2019-05-05 21:37:30 +0800

    Update 数值的整数次方.java
    给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。

--- Come_Offer/数值的整数次方.java
+++ Come_Offer/数值的整数次方.java
@@ -10,3 +10,29 @@ public double Power(double base, int exponent) {
   }
   总结
     1.直接写 double类型的结果利用 绝对值直接求出结果 如果为- 则取到数
+ public double Power(double base, int exponent) {
+        if(exponent==0&&base!=0){
+            return 1;
+        }
+        if(exponent==1){
+            return base;
+        }
+        if(base==0&&exponent<=0){
+            throw new RuntimeException();
+        }
+        if(base==0&&exponent>0){
+            return 0;
+        }
+        double result = Power(base,Math.abs(exponent)>>1);
+        result*=result;
+        if((exponent&1)==1){
+            result*=base;
+        }
+        if(exponent<0){
+            result = 1/result;
+        }
+        return result;
+  }
+  总结
+        1.考虑多种情况
+        base exponent 的组合  10 01 11 00 1大于0 0小于0
