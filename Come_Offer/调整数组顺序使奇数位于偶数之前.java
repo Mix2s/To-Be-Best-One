f2be1f15517b03543a01ae7361c99c92d8bbca7c
Mix2s <44645718+Mix2s@users.noreply.github.com>
2019-05-06 22:57:04 +0800

    Update 调整数组顺序使奇数位于偶数之前.java

--- Come_Offer/调整数组顺序使奇数位于偶数之前.java
+++ Come_Offer/调整数组顺序使奇数位于偶数之前.java
@@ -7,7 +7,31 @@
                    a[j-1]=tmp;
                }
            }
+
        }
     }
     总结
       1.采用冒泡思想 每一次交换奇数和偶数顺序 第一次循环将 最大奇数 放前 第二次 第二奇数。。。。
+ public void reOrderArray(int [] a) {
+      if(a.length==0){
+          return ;
+      }
+        int pre=0;
+        int cur=0;
+        int[] array = new int[a.length];
+        for(int i=0;i<a.length;i++){
+            if((a[i]&1)==1){
+                pre++;
+            }
+        }
+        for(int i=0;i<a.length;i++){
+            if(a[i]%2==1){
+                array[cur++]=a[i];
+            }else{
+                array[pre++]=a[i];
+            }
+        }
+        for(int i=0;i<a.length;i++){
+            a[i]=array[i];
+        }
+    }
