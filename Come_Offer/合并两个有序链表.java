15be4ece91dafd0f392bd5c14bf3ef9e03a20c92
Mix2s <44645718+Mix2s@users.noreply.github.com>
2019-05-08 21:40:06 +0800

    Update 合并两个有序链表.java

--- Come_Offer/合并两个有序链表.java
+++ Come_Offer/合并两个有序链表.java
@@ -24,4 +24,21 @@ public ListNode Merge(ListNode list1,ListNode list2) {
     }
     总结
       1.正常思维 创建一个新的链表 然后当传入的链表不为空时 进行比较当符合情况 head.next = list 移动head
-      
+   public ListNode Merge(ListNode list1,ListNode list2) {
+      if(list1==null){
+          return list2;
+      }
+       if(list2==null){
+           return list1;
+       }
+        if(list1.val<=list2.val){
+            list1.next=Merge(list1.next,list2);
+            return list1;
+        }else{
+            list2.next = Merge(list1,list2.next);
+             return list2;
+        }
+    }   
+总结
+       1.采用递归 if(kust1.val<list2.val){
+                     list1.next = 后面连接递归
