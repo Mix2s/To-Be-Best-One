ddf233ef7afefac0679cbe6b331cc2e74d1f8238
Mix2s <44645718+Mix2s@users.noreply.github.com>
2019-05-13 22:55:23 +0800

    Update 顺时针打印.java

--- Come_Offer/顺时针打印.java
+++ Come_Offer/顺时针打印.java
@@ -1,17 +1,57 @@
 package src.javaTest;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class day513 {
+    List a = new ArrayList();
     public static void main(String[] args) {
         int[][] array = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-        List ss = day513.printMatrix(array);
-        for (Object i:ss
-             ) {
-            System.out.print((int)i+" ");
-        }
+        day513 dd = new day513();
+        dd.print(array);
     }
 
+    public  ArrayList print(int[][] array) {
+        int tr = 0;
+        int tc = 0;   //tr tc 左上角初始值
+        int dr = array.length - 1;
+        int dc = array[0].length - 1;     //dr dc 右下角初始值
+        while (tr <= dr && tc <= dc) {  //左上角等于右下角时结束
+            printEdge(array, tr++, tc++, dr--, dc--);
+        }
+        return a;
+    }
+
+    public void printEdge(int[][] a,int tr,int tc,int dr,int dc){
+        if(tr==dr){  //当只有一行时候 把这一行打印出来
+            for(int i = tc;i<=dc;i++){
+                a.add(a[tr][i]);
+            }
+        }
+        if(tc==dc){ //当只有一列时候
+            for(int i=tr;i<=dr;i++){
+                a.add(a[i][tc]);
+            }
+        }
+        int curc = tc;
+        int curr = tr; //两个当前变量 存储当前位置
+        while(curc!=dc){
+            a.add(a[tr][curc]);
+            curc++;
+        }
+        while(curr!=dr){
+            a.add(a[curr][tc]);
+            curr++;
+        }
+        while(curc!=tc){
+            a.add(a[dr][curc]);
+            curc--;
+        }
+        while(curr!=tr){
+            a.add(a[curr][tc]);
+            curr--;
+        }
+    }
     public static ArrayList<Integer> printMatrix(int[][] array) {
         ArrayList<Integer> result = new ArrayList<Integer>();
         if (array.length == 0) return result;
