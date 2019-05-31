8017378d79cffc219db22c1b3c093b347a11a8cf
Mix2s <44645718+Mix2s@users.noreply.github.com>
2019-05-31 16:56:14 +0800

    Update 机器人走路最短路径.java

--- Come_Offer/机器人走路最短路径.java
+++ Come_Offer/机器人走路最短路径.java
@@ -37,3 +37,14 @@ public class que1 {
         }
     }
 }
+
+
+
+
+public class Robot {
+public int countWays(int x, int y) {
+if ( x==1 || y==1 )
+return 1;
+return countWays(x-1,y)+countWays(x,y-1);
+}
+}
