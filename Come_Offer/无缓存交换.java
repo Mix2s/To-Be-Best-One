c064d604497ee940cc8624c4a3afde7221f7eb1b
Mix2s <44645718+Mix2s@users.noreply.github.com>
2019-06-01 23:28:56 +0800

    Create 无缓存交换.java

---
+++ Come_Offer/无缓存交换.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Exchange {
+    public int[] exchangeAB(int[] AB) {
+           Stack<Integer> stack = new Stack();
+        stack.add(AB[0]);
+        stack.add(AB[1]);
+        AB[0] = stack.pop();
+        AB[1] = stack.pop();
+        return AB;
+        /*
+          AB[0]=AB[0]^AB[1];
+       AB[1]=AB[0]^AB[1];
+       AB[0]=AB[0]^AB[1];
+       return AB;*/
+        /*
+        AB[0]=AB[0]+AB[1];
+        AB[1]=AB[0]-AB[1];
+        AB[0]=AB[0]-AB[1];
+        return AB;*/
+    }
+}
