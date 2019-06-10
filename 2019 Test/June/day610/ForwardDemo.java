70b619c9da72348168a19ca4d32a8a8089bd2b8e
Mix2s <44645718+Mix2s@users.noreply.github.com>
2019-06-10 16:14:40 +0800

    Create ForwardDemo.java

---
+++ 2019 Test/June/day610/ForwardDemo.java
@@ -0,0 +1,22 @@
+package morning;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ForwardDemo extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletContext servletContext = this.getServletContext();
+        RequestDispatcher rd = servletContext.getRequestDispatcher("/intdex.html");
+        rd.forward(request,response);
+
+    }
+}
