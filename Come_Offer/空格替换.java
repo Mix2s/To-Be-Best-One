0f2ca3efd78122d3139cc82e2a4b1c31fdadec69
Mix2s <44645718+Mix2s@users.noreply.github.com>
2019-07-16 10:08:47 +0800

    Create 空格替换.java
    替换字符串中的空格 为 %20

---
+++ Come_Offer/空格替换.java
@@ -0,0 +1,7 @@
+public class Replacement {
+    public String replaceSpace(String iniString, int length) {
+         String str = iniString.replace(" ","%20");
+          System.out.println(str);
+        return str;
+    }
+}
