4995d63d08a4bec9956ba8a0c518f0bec08674ae
Mix2S <1097427662@qq.com>
2019-08-01 17:28:52 +0800

    基础性能测试框架 项目

---
+++ 2019_work/Performance testing framework/shuaihui/annotations/BenchMark.java
@@ -0,0 +1,13 @@
+package com.shuaihui.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface BenchMark {
+
+}
