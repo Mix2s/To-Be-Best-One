5eb1cbde15c39c935133d190cf4608a2a6b93bae
Mix2S <1097427662@qq.com>
2019-09-01 09:13:52 +0800

    delete

--- ChatRoom/chatroom/src/main/java/com/hui/client/service/privateChatGuI.java
+++
@@ -1,9 +0,0 @@
-package com.hui.client.service;
-
-import javax.swing.*;
-
-public class privateChatGuI {
-    private JPanel panel1;
-    private JTextArea textArea1;
-    private JTextArea textArea2;
-}
