3c8700d77e732d6c6d9f95c3cc007f32e8f9fb53
Mix2S <1097427662@qq.com>
2019-12-02 16:52:05 +0800

    day12.1

---
+++ 2019 Test/December/acturator/src/test/java/com/hui/acturator/ActuratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hui.acturator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
